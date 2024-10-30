1078a95bb3ca7b2ad0cb4d25983628a2e1138095
nthun <nagy.tamas@ppk.elte.hu>
2024-10-30 09:09:43 +0100

    Add models where data is only from 1 study

--- R/quick_analysis.R
+++ R/quick_analysis.R
@@ -12,8 +12,6 @@
 # Percomch – SST omissions neutral percent (post – pre)
 # Cort_ch – cortisol (post – pre)
 # 
-# 
-# 
 # EAS – temperamentum kérdoív: (ahol van a változó végén egy 2-es pl.: EAS_shy2 ott ki van véve egy tétel a jobb reliabilitás miatt)
 # EAS_emot – emocionalitás skála
 # EAS_acti – aktivitás skála
@@ -48,12 +46,16 @@ all_proc <-
   map(~mutate(.x, across(where(is.labelled), as_factor))) |> 
   map(~mutate(.x, across(any_of(c("ID", "id")), as.character))) |> 
   map(~rename(.x, cond = any_of(c("Cond", "cond")))) |>
+  map(~rename(.x, gender = any_of(c("nem", "gender", "Gender")))) |>
   map(~rename(.x, SDQ_kapcs = any_of(c("SDQ_kapcs", "SDQkapcs")))) |> 
   map(~rename(.x, SDQ_szoc = any_of(c("SDQ_szoc", "SDQszoc"))),
       ~rename(.x, HFflowch = any_of(c("HFflow_ch", "HFflowch")))) |> 
-    map(~mutate(.x, cond = case_when(cond %in% c("kontroll", "control") ~ "control",
+  map(~mutate(.x, cond = case_when(cond %in% c("kontroll", "control") ~ "control",
                                    cond %in% c("mindfulness", "intervention") ~ "mindfulness"
                                    ))) |> 
+  map(~mutate(.x, gender = case_when(gender %in% c("female", "girl", "lány") ~ "female",
+                                     gender %in% c("male", "boy", "fiú") ~ "male"
+  ))) |> 
   map2(.y = names(all_raw), ~mutate(.x, id = paste0(.y, "_", row_number()), .before = 1))
   
 # This is a super ugly solution to transform cortisol values in study 3 from µg/dL to ng/mL
@@ -61,14 +63,11 @@ all_proc$`3` <-
   all_proc$`3` |> 
   mutate(across(contains("cort"), ~`*`(., 10)))
 
-
 map(all_proc, glimpse)
 
 map(all_proc, ~pull(.x, cond))
 
-map(all_proc, ~select(.x, contains("cort")))
-
-
+map(all_proc, ~select(.x, contains(c("Loc", "Dir"))))
 
 # Multilevel models -------------------------------------------------------
 # A modelleket úgy építettem fel, hogy a moderátor az SDQ valamelyik faktora/összpontszám vagy az EAS valamelyik faktora, a függetletlen változó mindig a kondíció, és a függo változó a kortizolban, végrehajtókban bekövetkezett változás. Mindegyik kivont értéknél (change) az a jobb, ha minél kisebb, kivéve a Corsi backward span esetében (CBs_ch). 
@@ -82,7 +81,7 @@ mod_df <- map(all_proc, ~select(.x, any_of(c("id", "cond", outcomes, moderators)
 
 # Create models
 
-all_models <-
+alldata_df <- 
   mod_df |> 
   bind_rows(.id = "study") |> 
   # Standardize all numeric variables
@@ -96,13 +95,24 @@ all_models <-
   mutate(study_n = n_distinct(study)) |> 
   group_by(outcome, moderator, study_n) |> 
   nest() |> 
-  ungroup() |>
-  # Remove all models with less than 2 datasets
+  ungroup()
+  
+  
+
+lm_models <- 
+  alldata_df |> 
+  filter(study_n == 1) |> 
+  mutate(model = map(data, ~lm(outcome_value ~ cond * moderator_value, data = .x)))
+
+lmer_models <- 
+  alldata_df |>
   filter(study_n > 1) |> 
-  mutate(model = 
-           if_else(study_n > 1, 
-                   map(data, ~lmer(outcome_value ~ cond * moderator_value + (1|study), data = .x)),
-                   map(data, ~lm(outcome_value ~ cond * moderator_value, data = .x))),
+  mutate(model = map(data, ~lmer(outcome_value ~ cond * moderator_value + (1|study), data = .x)))
+
+# Extract model parameters, performance, and plots for each model
+all_models <- 
+  bind_rows(lm_models, lmer_models) |> 
+  mutate(
          estimates = map(model, tidy, conf.int = TRUE),
          performance = map(model, glance),
          plot = pmap(list(data, outcome, moderator), 
@@ -117,10 +127,11 @@ all_models <-
 
 
 all_models |> 
-  unnest(estimates) |> 
-  filter(effect == "fixed") |> 
-  select(outcome, moderator, study_n, term, estimate, conf.low, conf.high, std.error:p.value) |> 
+  unnest(estimates) |> view()
+  filter(effect != "ran_pars" | is.na(effect)) |> 
+  select(outcome, moderator, study_n, term, estimate, conf.low, conf.high, df = contains("df"), std.error:p.value) |> 
   # arrange(p.value) |> 
+  # mutate(p.value = p.adjust(p.value, "fdr")) |> 
   group_by(outcome, moderator) |>
   gt() |> 
   fmt_number(c("estimate", "std.error", "conf.low", "conf.high", "statistic", "df"), decimals = 2) |> 
@@ -159,6 +170,8 @@ plots |>
 # Correlations ------------------------------------------------------------
 # Megnezned lszi a korrelaciokat a moderatorok (1. temperamentum (EAS) skalak, 2. SDQ total es skalak) es a baseline (pre-test) kortizol es kognitiv teljesitmeny (1. CB, 2. HF mix, 3. HF flower, 4. shark) kozott?
 
+# Illetve a korrelációknál,  a kognitív tesztek közül hiányoznak a  CBs_ch, TMTe_ch, TMTt_ch, Loc_e_ch, Dir_e_ch, SSTneuch, Percomch
+  
 # HFflowch - Cort_ch
 # Shark_ch - Cort_ch
 # Shark_ch - HFflowch
@@ -177,12 +190,15 @@ correlators <- c("EAS_emot", "EAS_acti", "EAS_soc", "EAS_shy",
                  "SDQ_kapcs", "SDQ_szoc", "pre_CB_span", 
                  "pre_G_comission_error_shark_error",  "pre_HF_flowers_error",
                  "pre_HF_mix_error", "pre_cort_basal_mean",
-                 "Cort_ch", "HFflowch", "Shark_ch")
+                 "Cort_ch", "HFflowch", "Shark_ch",
+                 
+                 "CBs_ch", "TMTe_ch", "TMTt_ch", "Loc_e_ch", "Dir_e_ch", 
+                 "SSTneuch", "Percomch")
 
 all_proc |> 
   map(~select(.x, any_of(c("id", correlators)))) |> 
   bind_rows(.id = "study") |> 
-  correlation(p_adjust = "none", method = "spearman") |> 
+  correlation(p_adjust = "fdr", method = "spearman") |> 
   arrange(p) |> 
   drop_na(rho) |> 
   gt() |> 
@@ -194,7 +210,7 @@ all_proc |>
               column_labels.background.color = "lightgrey", 
               row.striping.background_color = "#EEEEEE", 
               row.striping.include_table_body = TRUE) |> 
-  gtsave("docs/correlations.docx") |>
+  # gtsave("docs/correlations.docx") |>
   force()
 
 all_proc |> 
@@ -205,9 +221,61 @@ all_proc |>
   gt() |> 
   fmt_number(-Parameter, decimals = 2) |> 
   sub_missing(missing_text = "") |> 
-  gtsave("docs/cor_matrix.docx")
+  # gtsave("docs/cor_matrix.docx") |> 
+  force()
 
 
+
+# Post-test cortisol models -----------------------------------------------------------
+
+s1 <- all_proc$`1`$Cort_ch
+s2 <- all_proc$`3`$Cort_ch
+
+cor
+
+
+
+
+
+
+# Demo data -----------------------------------------------------------------------------------
+
+# Gender
+
+map(all_proc, ~pull(.x, gender)) |> 
+  map(~mean(.== "male")) |> 
+  map(~round(., 3)) |> 
+  map(~`*`(., 100)) |> 
+  bind_rows(.id = "study")
   
+# Age
+
+map(all_proc, ~pull(.x, Életkor))
+
+
 
 # SANDBOX -------------------------------------------------------------------------------------
+
+age_data <- map(all_proc, ~select(.x, contains(c("Életkor", "age_months", "age"))))
+
+age_data |> 
+  map(nrow)
+
+age_data |> 
+  map(pull) |> 
+  map(mean, na.rm = TRUE) |> 
+  map(round, 1)
+
+age_data |> 
+  map(pull) |> 
+  map(sd, na.rm = TRUE) |> 
+  map(round, 1)
+
+
+mtcars |> 
+  mutate(study = 1) |> 
+  add_row(tibble_row(study = 2)) |> 
+  lmer(disp ~ mpg + (1|study), data = _)
+
+tidy.lm()
+         
